d1971be2325a6ee5bf974bd90939d1a2a8fa94aa
Anastasia <nastia.iv.91@gmail.com>
2021-01-08 21:14:02 -0800

    add burger working

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -3,6 +3,13 @@ var router = express.Router();
 var burger = require('../models/burger.js');
 
 router.get('/', function(req, res) {
+	res.redirect('/index');
+});
+
+// add a '/index/' endpoint that gets all the burgers
+// then renders the index file by passing in all the burgers
+// as an object for handlebars to use
+router.get('/index', function(req, res) {
 	burger.selectAll(function(data) {
 		var hbsObject = {burgers: data};
 		console.log(hbsObject);
@@ -10,20 +17,24 @@ router.get('/', function(req, res) {
 	});
 });
 
-router.post('/api/burgers', function(req, res) {
+// add a '/burgers/insertOne' endpoint that posts the 
+// burger name the user entered then as a callback it
+// redirects back to the /index route
+router.post('/burgers/insertOne', function(req, res) {
 	burger.insertOne(['burger_name', 'devoured'], [req.body.name, false], function() {
-		res.redirect('/');
+		res.redirect('/index');
 	});
 });
 
-router.put('/api/burgers/:id', function(req, res) {
+// add a '/burgers/updateOne/:id' route that updates
+// the status of the burger from being uneaten to eaten
+// then does a callback that redirects to the /index endpoint
+router.put('/burgers/updateOne/:id', function(req, res) {
 	var condition = 'id = ' + req.params.id;
 	console.log('condition', condition);
 
-	burger.updateOne({
-		devoured: req.body.devoured}, condition, function(data) {
-			console.log(data)
-		res.redirect('/');
+	burger.updateOne({devoured: req.body.devoured}, condition, function() {
+		res.redirect('/index');
 	});
 });
 
--- public/assets/css/burg.js
+++ public/assets/css/burg.js
@@ -1,19 +1,19 @@
-// Send the POST request.
-$(document).on("ready", function(){ 
-$("#btn-default").on("click", function(event) {
+// Send the PUT request.
+// $(document).on("ready", function(){ 
+$("#button").on("click", function(event) {
     event.preventDefault();
-    alert( "Handler for .click() called." );
-//     console.log("I am here");
-//   var id = $(this).data("id");
-// $.ajax("/api/burgers/"+id, {
-//     type: "PUT",
-//     data: {"devoured":true}
-//   }).then(
-//     function() {
-//       console.log("created new burger");
-//     }
-//   );
-})  })
+    console.log("I am here");
+  var id = $(this).data("id");
+$.ajax("/api/burgers/"+id, {
+    type: "PUT",
+    data: {"devoured":true}
+  }).then(
+    function() {
+      console.log("created new burger");
+    }
+  );
+})  
+// })
 //   
       // Reload the page to get the updated list
     //   location.reload();
