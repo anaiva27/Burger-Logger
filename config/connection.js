7efdddc784b015efe6ff8bd727fa2d77f7d95e37
Anastasia <nastia.iv.91@gmail.com>
2021-01-09 15:59:44 -0800

    connecting JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,27 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+// connection for MySQL
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "27qweasd",
+//   database: "burgers_db"
+// });
+
+// connection if JawsDB is being used
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "27qweasd",
   database: "burgers_db"
 });
+}
 
 // Make connection.
 connection.connect(function(err) {
