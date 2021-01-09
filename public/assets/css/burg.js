9887fd5a617820c84c6724548031d96554998946
Anastasia <nastia.iv.91@gmail.com>
2021-01-09 11:53:18 -0800

    adding id's for listeners

--- public/assets/css/burg.js
+++ public/assets/css/burg.js
@@ -3,14 +3,15 @@ $("#button").on("click", function(event) {
     event.preventDefault();
     console.log("I am here");
   
-  var id = $(this).data("id");  console.log(id)
+  var id = $(this).closest("#list").data("id"); 
+   console.log(id)
 $.ajax("/burgers/updateOne/"+id, {
     type: "PUT",
     data: {"devoured":true}
   }).then(
     function() {
       console.log("you ate da burger");
-     location.reload();
+    //  location.reload();
     }
   );
 })  
